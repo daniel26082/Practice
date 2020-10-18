92bac8840251d16feac0e2fd87af76795df6b677
Daniel Choung <553021@my.cuhsd.org>
2020-10-18 22:34:07 +0000

    Added function draw_y_axis

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ trtl.pendown()
 for i in range(6):
   trtl.forward(10)
   trtl.left(15)
+trtl.penup()
+
+def draw_y_axis():
+  trtl.goto(0,0)
+  trtl.pendown()
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.penup()
+
+draw_y_axis()
