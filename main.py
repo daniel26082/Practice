435ca9f5b01b454a072b21bf362254537aaa2a99
Daniel Choung <553021@my.cuhsd.org>
2020-10-18 22:30:25 +0000

    Added function draw_x_axis

--- main.py
+++ main.py
@@ -14,4 +14,13 @@ def draw_y_axis():
   trtl.backward(200)
   trtl.penup()
 
+def draw_x_axis():
+  trtl.goto(0,0)
+  trtl.left(90)
+  trtl.pendown()
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.penup()
+
 draw_y_axis()
+draw_x_axis()
